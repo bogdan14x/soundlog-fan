b63d0af14e3a19640193f38228e83526e17b767f
Boggy B <bbogdan14@gmail.com>
2024-08-03 16:40:30 +0100

    update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,38 @@
-import ArtistPage from "@/ArtistPage";
-import getSoundlog from "@/server/getSoundlog";
-import { Metadata } from "next";
+import ArtistPage from '@/ArtistPage';
+import getSoundlog from '@/server/getSoundlog';
+import { Metadata } from 'next';
+import { headers } from 'next/headers';
 
 export async function generateMetadata(): Promise<Metadata> {
-	const { soundlog, artist } = await getSoundlog();
+  const { soundlog, artist } = await getSoundlog();
 
-	return {
-		title: `${soundlog.artist_name} on Soundlog`,
-		description: artist.bio,
-		twitter: {
-			title: `${soundlog.artist_name} on Soundlog`,
-			description: artist.bio,
-			images: [soundlog.cover_image],
-		},
-		openGraph: {
-			images: [soundlog.cover_image],
-		},
-	};
+  const forwardedHost = headers().get('x-forwarded-host');
+  const soundlogId = forwardedHost?.split('.')[0] ?? null;
+
+  return {
+    title: `${soundlog.artist_name} on Soundlog`,
+    description: `Discover ${soundlog.artist_name} on Soundlog`,
+    twitter: {
+      title: `${soundlog.artist_name} on Soundlog`,
+      description: artist.bio,
+      images: [soundlog.cover_image],
+    },
+    openGraph: {
+      type: 'profile',
+      url: soundlogId ? `https://${soundlogId}.soundlog.link` : "https://soundlog.app",
+      title: `${soundlog.artist_name} on Soundlog`,
+      description: `Discover ${soundlog.artist_name} on Soundlog`,
+      siteName: `Soundlog | Build Your Artist Page`,
+      images: [
+        {
+          url: soundlog.cover_image,
+        },
+      ],
+    },
+  };
 }
 
 export default async function Home() {
-	const data = await getSoundlog();
-	return <ArtistPage {...data} />;
+  const data = await getSoundlog();
+  return <ArtistPage {...data} />;
 }
